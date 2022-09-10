53a79dc93807a999e0369675e215eee808a37591
fatihmaras <fatihmmaras@gmail.com>
2022-09-11 02:22:13 +0300

    Added Class, Encapsulation

---
+++ OOP-1_Encapsulation/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_AddClass
+{
+    class Employee
+    {
+        private string _firstName;
+        private string _lastName;  //ctrl ve . tuşlarına nasılır , kapsülle..
+        public string FirstName
+        {
+            set 
+            {
+                _firstName = value;
+            }
+            get 
+            {
+                return _firstName;
+            }
+        }
+
+        private List<Employee> _lists;
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+        public int Age { get; set; }
+
+        public Employee()  
+        {
+
+        }
+
+
+
+        // Overloading  ----- Opsiyon 2
+        public Employee(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            // this ifadesi zorunlu değil.
+        }
+
+        // Overloading  ----- Opsiyon 1 
+        //public Employee(string firstName, string lastName, int age)  
+        //{
+        //    this.FirstName = firstName;
+        //    this.LastName = lastName;   
+        //    this.Age = age;
+
+
+
+       
+    }
+}
+
---
+++ OOP-1_Encapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_AddClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Ahmet";   // set bloğu çalışır
+            employee.LastName = "Türkoğlu";
+            employee.Age = 25;
+            Console.WriteLine(employee.FirstName); // get bloğu çalışır.
+            Console.WriteLine(employee.LastName);
+            Console.WriteLine(employee.Age);
+
+            Console.WriteLine("\n");
+
+            Employee employee2 = new Employee("Mehmet", "Üstün", 18);
+
+            Console.WriteLine(employee2.FirstName);
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
